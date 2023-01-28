042bafbb637f32effd4ce2c8c535012cf2c821a7
Comamoca <comamoca.dev@gmail.com>
2023-01-28 13:22:17 +0900

    :beetle: (main.ts) Fixed API Internal Error caused by MaxCDN shutdown

--- main.ts
+++ main.ts
@@ -1,30 +1,42 @@
-import { Application, Router } from 'oak'
-import 	{ parse } from "twemoji-parser"
+import { Application, Router } from "oak";
+import twemoji from "npm:@twemoji/api";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 function emoji2Url(emoji: string): string {
-	const entities = parse(emoji)
-	return entities[0].url
+  const html = twemoji.parse(emoji, {
+    folder: "svg",
+    ext: ".svg",
+  });
+  const docment = new DOMParser().parseFromString(html, "text/html");
+  const url = docment?.querySelector("img")?.getAttribute("src");
+
+  if (url == null) {
+    return "";
+  }
+
+  return url;
 }
 
 const app = new Application();
-const router = new Router()
+const router = new Router();
 
 router.get("/", async (ctx) => {
-	const htmlUrl = new URL("index.html", import.meta.url).toString();
-	const res = await fetch(htmlUrl)
-	ctx.response.body = res.body
-})
+  const htmlUrl = new URL("index.html", import.meta.url).toString();
+  const res = await fetch(htmlUrl);
+  ctx.response.body = res.body;
+});
 
 router.get("/api/:emoji", async (ctx) => {
-	console.log(emoji2Url(ctx.params.emoji))
-	const response = await fetch(emoji2Url(ctx.params.emoji))
-	ctx.response.headers.set('Content-Type', 'image/svg+xml')
-	// ctx.response.headers.set('Content-Type', 'text')
-	const svg = await response.text()
-	ctx.response.body = svg
-})
+  const emoji = ctx.params.emoji;
+
+  const response = await fetch(emoji2Url(emoji));
+  ctx.response.headers.set("Content-Type", "image/svg+xml");
+  const svg = await response.text();
+
+  ctx.response.body = svg;
+});
 
 app.use(router.routes());
-app.use(router.allowedMethods())
+app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
