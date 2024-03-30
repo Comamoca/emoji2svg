9d253e26a5a0ff4fa2fd693a8df09c90d27285ae
Comamoca <comamoca.dev@gmail.com>
2024-03-31 08:59:14 +0900

    :sparkles: (main.ts) Add sentry.

--- main.ts
+++ main.ts
@@ -2,6 +2,17 @@ import { Hono } from "hono";
 import twemoji from "twemoji";
 import { DOMParser } from "domparser";
 import { serveStatic } from "middleware";
+import * as Sentry from "sentry";
+import "dotenv";
+
+if (typeof Deno.env.get("SENTRY_DSN") !== "undefined") {
+  const dsn = Deno.env.get("SENTRY_DSN");
+  console.log(`Sentry DSN: ${dsn}`);
+
+  Sentry.init({
+    dsn: dsn,
+  });
+}
 
 function emoji2Url(emoji: string): string {
   const html = twemoji.parse(emoji, {
